0866cba8b11e66c33bfc681b785b8ec9dea69d5e
Rico Apon <rico.apon@gmail.com>
2022-02-24 21:04:37 +0100

    Implement dummy command that returns a lot of lines

--- src/app/backend/controller.ts
+++ src/app/backend/controller.ts
@@ -1,11 +1,15 @@
+import {InputCommand, CommandResponse} from './types/command-types';
+
 /**
  * This class is the entry point for the entire backend. Commands are inserted here.
  */
-import {InputCommand, CommandResponse} from './types/command-types';
-
 export class Controller {
-
   public parseCommand(command: InputCommand): CommandResponse {
+    if (command.command === 'ls') {
+      return {
+        response: 'line\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\n',
+      };
+    }
     return {response: 'Unknown command'};
   }
 }
