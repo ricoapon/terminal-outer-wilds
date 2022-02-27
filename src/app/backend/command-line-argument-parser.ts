70b569f60e8748ffdc3bb84c8e878cf0dbdd813d
Rico Apon <rico.apon@gmail.com>
2022-02-27 20:16:25 +0100

    Add utility method for parsing arguments

---
+++ src/app/backend/command-line-argument-parser.ts
@@ -0,0 +1,26 @@
+export type ParsedArgs = {
+  /** The first command in the line. */
+  mainCommand: string,
+  /** Any argument that starts with '-'. */
+  flags: Set<string>,
+  /** Any other argument, in order, that does not start with '-'. */
+  args: string[],
+};
+
+/** Parses given arguments into ParsedArgs object. */
+export function parseCommandLineArguments(args: string): ParsedArgs {
+  const splitArgs = args.split(' ');
+  const mainCommand = splitArgs.shift();
+  const flags: Set<string> = new Set<string>();
+  for (const arg of splitArgs) {
+    if (arg.startsWith('-')) {
+      flags.add(arg.substr(1));
+    }
+  }
+
+  return {
+    mainCommand,
+    flags,
+    args: splitArgs.filter(arg => !arg.startsWith('-')),
+  };
+}
