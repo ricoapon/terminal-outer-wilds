a06947130406fe6a50b0467372aa09c050e48f8c
Rico Apon <rico.apon@gmail.com>
2022-02-27 21:59:34 +0100

    Add npc text that can be added later on

--- src/app/backend/file-system/initializers/kingdom-far-far-away/shortcut-npc.ts
+++ src/app/backend/file-system/initializers/kingdom-far-far-away/shortcut-npc.ts
@@ -4,6 +4,14 @@ import {ParsedArgs} from '../../../util/command-line-argument-parser';
 
 export class ShortcutNpc implements Program {
   execute(parsedArgs: ParsedArgs): CommandResponse {
+    // When money something is implemented, make sure that the response gives the shortcut.
+    // noinspection PointlessBooleanExpressionJS
+    if (false) {
+      // noinspection UnreachableCodeJS
+      return {response:
+          'Money looses the tongue! The shortcut is an invisible directory, called shortcut, which directly goes to the city.\n' +
+          'Try the command `cd shortcut/city` next time.'};
+    }
     return {
       response: 'You look tired from walking all this way! Didn\'t you know about the shortcut? Sucks to be you. \n' +
         'I know about the shortcut, but I am not going to tell you for free.'
