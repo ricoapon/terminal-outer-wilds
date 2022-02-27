a76457128deb7937e79e8639afc250938753e519
Rico Apon <rico.apon@gmail.com>
2022-02-27 22:22:54 +0100

    Remove dummy files

--- src/app/backend/file-system/initializers/file-system-initializers.module.ts
+++ src/app/backend/file-system/initializers/file-system-initializers.module.ts
@@ -1,6 +1,5 @@
 import {InjectionToken, NgModule} from '@angular/core';
 import {FileSystemInitializer} from './file-system-initializer';
-import {DummyFiles} from './dummy-files';
 import {Root} from './root';
 import {Tutorial} from './tutorial/tutorial';
 import {KingdomFarFarAway} from './kingdom-far-far-away/kingdom-far-far-away';
@@ -12,7 +11,6 @@ export const FILE_SYSTEM_INITIALIZERS = new InjectionToken<FileSystemInitializer
   declarations: [],
   imports: [],
   providers: [
-    {provide: FILE_SYSTEM_INITIALIZERS, useClass: DummyFiles, multi: true},
     {provide: FILE_SYSTEM_INITIALIZERS, useClass: Root, multi: true},
     {provide: FILE_SYSTEM_INITIALIZERS, useClass: Tutorial, multi: true},
     {provide: FILE_SYSTEM_INITIALIZERS, useClass: KingdomFarFarAway, multi: true},
