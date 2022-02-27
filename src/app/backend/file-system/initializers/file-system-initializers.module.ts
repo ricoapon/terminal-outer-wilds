03bfd3571ab3962180f8e87a093c1c56d7848d46
Rico Apon <rico.apon@gmail.com>
2022-02-27 21:55:49 +0100

    Remove puzzle that is integrated in kingdom-far-far-away shortcut

--- src/app/backend/file-system/initializers/file-system-initializers.module.ts
+++ src/app/backend/file-system/initializers/file-system-initializers.module.ts
@@ -1,7 +1,6 @@
 import {InjectionToken, NgModule} from '@angular/core';
 import {FileSystemInitializer} from './file-system-initializer';
 import {DummyFiles} from './dummy-files';
-import {Puzzle2InvisibleDir} from './puzzle2-invisible-dir';
 import {Puzzle1Maze} from './puzzle-1-maze';
 import {Root} from './root';
 import {Tutorial} from './tutorial';
@@ -14,7 +13,6 @@ export const FILE_SYSTEM_INITIALIZERS = new InjectionToken<FileSystemInitializer
   imports: [],
   providers: [
     {provide: FILE_SYSTEM_INITIALIZERS, useClass: DummyFiles, multi: true},
-    {provide: FILE_SYSTEM_INITIALIZERS, useClass: Puzzle2InvisibleDir, multi: true},
     {provide: FILE_SYSTEM_INITIALIZERS, useClass: Puzzle1Maze, multi: true},
     {provide: FILE_SYSTEM_INITIALIZERS, useClass: Root, multi: true},
     {provide: FILE_SYSTEM_INITIALIZERS, useClass: Tutorial, multi: true},
