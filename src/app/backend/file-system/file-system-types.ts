9a59ce113f67d038e9f48b2ee8877632cf5c9d9b
Rico Apon <rico.apon@gmail.com>
2022-02-27 12:50:37 +0100

    Add types used in the file system

---
+++ src/app/backend/file-system/file-system-types.ts
@@ -0,0 +1,67 @@
+import {Path} from './path';
+
+export interface FileSystemNode {
+  name(): string;
+  isDirectory(): boolean;
+}
+
+export class File implements FileSystemNode {
+  private readonly _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+  }
+
+  name(): string {
+    return this._name;
+  }
+
+  isDirectory(): boolean {
+    return false;
+  }
+}
+
+export class Directory implements FileSystemNode {
+  private readonly _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+  }
+
+  name(): string {
+    return this._name;
+  }
+
+  isDirectory(): boolean {
+    return true;
+  }
+}
+
+export class SymbolicLinkToDirectory implements FileSystemNode {
+  private readonly _name: string;
+  private readonly _pointsTo: Path;
+
+  constructor(name: string, pointsTo: Path) {
+    this._name = name;
+    this._pointsTo = pointsTo;
+  }
+
+  name(): string {
+    return this._name;
+  }
+
+  isDirectory(): boolean {
+    return true;
+  }
+
+  /** Returns the absolute path of the directory that the symbolic link points to. */
+  pointsTo(): Path {
+    return this._pointsTo;
+  }
+}
+
+export class DirectoryProperties {
+  owner(): string {
+    return 'root';
+  }
+}
