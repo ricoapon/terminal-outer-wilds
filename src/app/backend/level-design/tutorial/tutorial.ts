506e5be2bfced72420a6c1a5841db105d4952748
Rico Apon <rico.apon@gmail.com>
2022-03-02 23:17:26 +0100

    Add video in the tutorial for testing

--- src/app/backend/level-design/tutorial/tutorial.ts
+++ src/app/backend/level-design/tutorial/tutorial.ts
@@ -1,5 +1,5 @@
 import {AbsolutePath} from '../../in-memory-file-system/paths';
-import {Directory, DirectoryProperties, InMemoryFile, ProgramFile} from '../../in-memory-file-system/file-system-types';
+import {Directory, DirectoryProperties, InMemoryFile, ProgramFile, VideoFile} from '../../in-memory-file-system/file-system-types';
 import {Injectable} from '@angular/core';
 import {InMemoryFileSystemFacade} from '../../in-memory-file-system/in-memory-file-system-facade';
 import {LevelDesigner} from '../level-designer';
@@ -17,6 +17,15 @@ export class Tutorial implements LevelDesigner {
 
   design(startingPath: AbsolutePath, fileSystem: InMemoryFileSystemFacade): void {
     fileSystem.createNode(startingPath, new InMemoryFile('step-1.txt', 'tutorial/step-1.txt'));
+    fileSystem.createNode(startingPath, new VideoFile('some-step', [
+      {location: '/', command: 'cd tutorial'},
+      {location: 'tutorial', command: 'cd step-2'},
+      {location: 'step-2', command: 'execute npc.sh'},
+      {response: 'I am an npc'},
+      {location: 'step-2', command: 'cd /'},
+      {location: 'step-2', command: 'execute /tutorial/step-2/npc.sh'},
+      {response: 'I am an npc'},
+    ]));
 
     fileSystem.createNode(startingPath, new Directory('step-2'));
     startingPath = startingPath.resolve('step-2');
