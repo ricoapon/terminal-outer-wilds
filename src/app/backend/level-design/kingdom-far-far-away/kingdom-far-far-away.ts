8ddb37eee4652d40b34afcc04faf7e0f7b9b3386
Rico Apon <rico.apon@gmail.com>
2022-03-02 18:46:05 +0100

    Implement kingdom-far-far-away

--- src/app/backend/level-design/kingdom-far-far-away/kingdom-far-far-away.ts
+++ src/app/backend/level-design/kingdom-far-far-away/kingdom-far-far-away.ts
@@ -2,6 +2,8 @@ import {LevelDesigner} from '../level-designer';
 import {Injectable} from '@angular/core';
 import {InMemoryFileSystemFacade} from '../../in-memory-file-system/in-memory-file-system-facade';
 import {AbsolutePath} from '../../in-memory-file-system/paths';
+import {Directory, DirectoryProperties, ProgramFile, SymbolicLinkToDirectory} from '../../in-memory-file-system/file-system-types';
+import {ShortcutNpc} from './shortcut-npc';
 
 @Injectable()
 export class KingdomFarFarAway implements LevelDesigner {
@@ -10,5 +12,24 @@ export class KingdomFarFarAway implements LevelDesigner {
   }
 
   design(startingPath: AbsolutePath, fileSystem: InMemoryFileSystemFacade): void {
+    let farFarAwayPath = startingPath;
+
+    // Create the invisible shortcut with the city.
+    fileSystem.createNode(startingPath, new Directory('shortcut', new DirectoryProperties(undefined, true)));
+    startingPath = startingPath.resolve('shortcut');
+    fileSystem.createNode(startingPath, new Directory('city'));
+    startingPath = startingPath.resolve('city');
+
+    // Create directories far/far/far/away/city where city symlinks to the actual city.
+    for (let i = 0; i < 10; i++) {
+      fileSystem.createNode(farFarAwayPath, new Directory('far'));
+      farFarAwayPath = farFarAwayPath.resolve('far');
+    }
+    fileSystem.createNode(farFarAwayPath, new Directory('away'));
+    farFarAwayPath = farFarAwayPath.resolve('away');
+    fileSystem.createNode(farFarAwayPath, new SymbolicLinkToDirectory('city', startingPath));
+
+    // Create the content of the city.
+    fileSystem.createNode(startingPath, new ProgramFile('vvv', new ShortcutNpc()));
   }
 }
